afd1a0780ddc84a7410c0874fe8e5346433d2ee1
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-03-21 21:34:58 -0300

    theme styled

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+
 import { GlobalStyles } from './styles/global-styles';
+import { theme } from './styles/theme';
+
 import Home from './templates/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Home />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
 );
