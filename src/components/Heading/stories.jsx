4a3b932b7db383292d1c3475efabcb651e347a25
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-03-28 20:36:02 -0300

    dark and light pages

--- src/components/Heading/stories.jsx
+++ src/components/Heading/stories.jsx
@@ -5,11 +5,30 @@ export default {
   title: 'Heading',
   component: Heading,
   args: {
-    children: 'Qualquer coisa',
+    children: 'O texto está escuro',
+    light: false,
   },
   argTypes: {
     children: {type: 'string'},
+    light: {type: 'boolean'},
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
   },
 };
 
-export const Template = (args) => <Heading {...args} />;
+export const Light = (args) => <Heading {...args} />;
+export const Dark = (args) => <Heading {...args} light={true} />;
+
+Light.parameters = {
+  backgrounds: {
+    default: 'light',
+  },
+};
+
+Dark.args = {
+  children: 'O texto está claro',
+  light: true,
+};
