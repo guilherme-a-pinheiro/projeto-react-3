a2c2ceb2fc003b3d1ded99649a4eda8b717774fc
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-03-30 22:32:07 -0300

    finishing heading

--- src/components/Heading/stories.jsx
+++ src/components/Heading/stories.jsx
@@ -6,11 +6,9 @@ export default {
   component: Heading,
   args: {
     children: 'O texto está escuro',
-    light: false,
   },
   argTypes: {
     children: {type: 'string'},
-    light: {type: 'boolean'},
   },
   parameters: {
     backgrounds: {
@@ -20,7 +18,7 @@ export default {
 };
 
 export const Light = (args) => <Heading {...args} />;
-export const Dark = (args) => <Heading {...args} light={true} />;
+export const Dark = (args) => <Heading {...args} />;
 
 Light.parameters = {
   backgrounds: {
@@ -30,5 +28,5 @@ Light.parameters = {
 
 Dark.args = {
   children: 'O texto está claro',
-  light: true,
+  colorDark: false,
 };
